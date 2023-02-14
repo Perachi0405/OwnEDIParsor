345c45f32f9143cc536ebeb74b771ae26da04634
Perachi0405 <Deva.Anbu@rentacenter.com>
2023-02-14 20:41:32 +0530

    Updated the docker file

--- docker.sh
+++ docker.sh
@@ -24,20 +24,20 @@ cd $(git rev-parse --show-toplevel)
 function build() {
   green_printf "Before building, first cleaning... \n"
   clean || panic_fail_op
-  green_printf "Building omniparser image ... \n"
+  green_printf "Building ownediparse image ... \n"
   docker build -t ownediparse-cli-server . || panic_fail_op
-  green_printf "Launching omniparser ... \n"
-  docker run -d -p 8080:8080 omniparser-cli-server || panic_fail_op
-  green_printf "Showing omniparser logs ... \n"
-  docker ps --filter="ancestor=omniparser-cli-server" --format="{{.ID}}" | xargs docker logs -f
+  green_printf "Launching ownediparse ... \n"
+  docker run -d -p 8080:8080 ownediparse-cli-server || panic_fail_op
+  green_printf "Showing ownediparse logs ... \n"
+  docker ps --filter="ancestor=ownediparse-cli-server" --format="{{.ID}}" | xargs docker logs -f
 }
 
 function clean() {
-  green_printf "Stopping/removing all omniparser containers  ... \n"
-  docker ps --filter="ancestor=omniparser-cli-server" --format="{{.ID}}" | \
+  green_printf "Stopping/removing all ownediparse containers  ... \n"
+  docker ps --filter="ancestor=ownediparse-cli-server" --format="{{.ID}}" | \
       xargs docker rm --force || true
-  green_printf "Removing all omniparser images  ... \n"
-  docker images --filter "reference=omniparser-cli-server:*" --format "{{.ID}}" | \
+  green_printf "Removing all ownediparse images  ... \n"
+  docker images --filter "reference=ownediparse-cli-server:*" --format "{{.ID}}" | \
       xargs docker rmi --force || true
   green_printf "Cleaning complete.\n"
 }
